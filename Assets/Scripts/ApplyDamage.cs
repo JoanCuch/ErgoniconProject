db82411f14ec665178f35ea8ba48f3c73138cb53
Joan Cuch <joancuch@gmail.com>
2020-01-04 13:34:08 +0100

    First Stage VR Ready

---
+++ Assets/Scripts/ApplyDamage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyDamage : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision collision)
+	{
+		collision.gameObject.SendMessageUpwards("ApplyDamage", SendMessageOptions.DontRequireReceiver);
+	}
+}
---
+++ Assets/Scripts/SimpleAttach.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class SimpleAttach : MonoBehaviour
+{
+
+	private Interactable interactable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		interactable = GetComponent<Interactable>();
+    }
+
+    private void OnHandHoverBegin(Hand hand)
+	{
+		hand.ShowGrabHint();
+	}
+
+	private void OnHandHoverEnd(Hand hand)
+	{
+		hand.HideGrabHint();
+	}
+
+	private void HandHoverUpdate(Hand hand)
+	{
+		GrabTypes grabType = hand.GetGrabStarting();
+		bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+		//Grab the object
+		if(interactable.attachedToHand == null && grabType != GrabTypes.None)
+		{
+			hand.AttachObject(gameObject, grabType);
+			hand.HoverLock(interactable);
+		}
+		//Release
+		else if (isGrabEnding)
+		{
+			hand.DetachObject(gameObject);
+			hand.HoverUnlock(interactable);
+		}
+	}
+}
